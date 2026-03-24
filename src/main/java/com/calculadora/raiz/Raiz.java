232679911078e7c1362dfb27bb093aeb74926183
José Juan Ibáñez Cemborain <josejuan.ic.dim@gmail.com>
2026-03-24 19:54:06 +0100

    Commit: limpio de spring

--- src/main/java/com/calculadora/raiz/Raiz.java
+++ src/main/java/com/calculadora/raiz/Raiz.java
@@ -1,12 +1,7 @@
 package com.calculadora.raiz;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class Raiz {
-
-	public static double raiz(double numero) {
-	      return Math.sqrt(numero);
-	   }
-	
+    public static double raiz(double numero) {
+        return Math.sqrt(numero);
+    }
 }
